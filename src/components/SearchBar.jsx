084035c4f0515a344e5d3c0047b32338839e8fca
prashantkumar182000 <prashantkumar18.pk@gmail.com>
2023-05-30 00:19:03 +0530

    first commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { Paper, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm('');
+    }
+  };
+
+  return (
+    <Paper
+      component='form'
+      onSubmit={onhandleSubmit}
+      sx={{
+        borderRadius: 10,
+        border: '1px solid #e3e3e3',
+        pl: 3,
+        boxShadow: 'none',
+        mr: { sm: 1 },
+      }}
+    >
+      <input
+        className='search-bar'
+        placeholder='Search anything ...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton type='submit' sx={{ p: '10px', color: 'black' }} aria-label='search'>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
+
+export default SearchBar;
---
+++ src/utils/constants.js
@@ -0,0 +1,39 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/GV0gdC7/Youtube-logo.png';
+
+export const categories = [
+  { name: 'Home', icon: <HomeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Learning', icon: <CodeIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/23pHb7K/youtube-logo-dark.jpg';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'Youtube 2.0';
+export const demoVideoTitle = 'Gro.care Assignment';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
